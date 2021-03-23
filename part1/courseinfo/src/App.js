80278076519c5d2acad0aad42f457b225362bd06
DAVID-LAPTOP\david <davidgl1987@gmail.com>
2021-03-23 20:48:07 +0100

    part 5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -23,28 +23,30 @@ const Total = ({exercises}) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  const exercises = parts.reduce((acc, part) => acc + part.exercises, 0);
+  const exercises = course.parts.reduce((acc, part) => acc + part.exercises, 0);
   
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total exercises={exercises} />
     </div>
   )
